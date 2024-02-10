1e9ac0908b59bd7f23df7f7e541f179c27c78ae6
Barzin <barzin.development@gmail.com>
2024-02-10 16:59:31 +0100

    Setup farm flask app. First commit.

---
+++ farm/main.py
@@ -0,0 +1,15 @@
+# Import what we need from flask
+from flask import Flask, render_template
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
