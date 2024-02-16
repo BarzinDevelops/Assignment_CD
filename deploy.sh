ec1245eec843074883e57d1d44888e80b0e18978
Barzin <barzin.development@gmail.com>
2024-02-16 19:27:56 +0100

    Adjust deploy.sh file to have the right path to vps repo.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+REPO_PATH="/home/github/Assignment_CD"  # Adjust this path to your repository location
+
 SSH_PRIVATE_KEY="$HOME/.ssh/DigitalOcean_ssh_barrybuntu/id_rsa"
 
 echo "Deploying to VPS..."
 
+
 # SSH into VPS and update code. Use known_hosts file to verify the host key
-ssh -o StrictHostKeyChecking=yes -o UserKnownHostsFile=$HOME/.ssh/known_hosts -i "$SSH_PRIVATE_KEY" root@165.22.89.227 'cd /home/farm/ && git pull origin main'
+ssh -o StrictHostKeyChecking=yes -o UserKnownHostsFile=$HOME/.ssh/known_hosts -i "$SSH_PRIVATE_KEY" root@165.22.89.227 "cd $REPO_PATH && git pull origin main"
