f759037966713937b3b017cecb7fc2085fdb6f74
Barzin <barzin.development@gmail.com>
2024-02-16 13:12:12 +0100

    Add vps servers hostkey to known_hosts and in deploy.sh file Use known_hosts file to verify the host key.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ SSH_PRIVATE_KEY="$HOME/.ssh/DigitalOcean_ssh_barrybuntu/id_rsa"
 
 echo "Deploying to VPS..."
 
-# SSH into VPS and update code
-ssh -i "$SSH_PRIVATE_KEY" user@165.22.89.227 'cd /home/farm/ && git pull origin main'
-
+# SSH into VPS and update code. Use known_hosts file to verify the host key
+ssh -o StrictHostKeyChecking=yes -o UserKnownHostsFile=$HOME/.ssh/known_hosts -i "$SSH_PRIVATE_KEY" user@165.22.89.227 'cd /home/farm/ && git pull origin main'
